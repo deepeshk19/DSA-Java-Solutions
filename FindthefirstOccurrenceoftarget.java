faca5cc0a382bca2b67c0e6ef2d8418e2c006440
Deepesh <deepeshkunkulol45@gmail.com>
2026-04-03 13:19:10 +0530

    Find the first Occurrence of target element

---
+++ FindthefirstOccurrenceoftarget.java
@@ -0,0 +1,26 @@
+// Find the first Occurrence of target element
+
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,2,3,4};
+        int target = 2;
+        
+        int left = 0;
+        int right = arr.length - 1;
+        int result = -1;
+        
+        while(left<=right) {
+            int mid = left + (right - left)/2;
+            
+            if(target == arr[mid]) {
+                result = mid;
+                right = mid - 1;
+            } else if(arr[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        System.out.println(result);
+    }
+}
