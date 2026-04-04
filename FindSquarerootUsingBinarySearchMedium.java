57d36e1f6bbd89ee5f16c32c0276b269666cee56
Deepesh <deepeshkunkulol45@gmail.com>
2026-04-04 14:28:14 +0530

    Find Squareroot Using Binary Search Medium Level

---
+++ FindSquarerootUsingBinarySearchMedium.java
@@ -0,0 +1,26 @@
+// Find Squareroot for the given number 
+
+class Main {
+    public static void main(String[] args) {
+        int x = 50;
+
+        int left = 0, right = x;
+        int result = -1;
+        
+        while(left <= right) {
+            int mid = left + (right - left)/2;
+            long square = mid * mid;
+            
+            if(square == x) {
+                result = mid;
+                break;
+            } else if(square <= x){
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        System.out.println(result);
+    }
+}
