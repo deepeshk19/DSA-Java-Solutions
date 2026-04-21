36cb26145135ab86902251fb25d027eb1f5d0e85
Deepesh <deepeshkunkulol45@gmail.com>
2026-04-21 13:12:39 +0530

    Longest Substring with sum < K

---
+++ LongestSubstringWithSumLessthanK.java
@@ -0,0 +1,24 @@
+// longest substring with sum < = k
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 1, 7, 10};
+        int k = 14;
+        
+        int left = 0, sum = 0, maxSum = 0;
+        
+        for(int right = 0; right < arr.length-1; right++) {
+            sum = sum + arr[right];
+            
+            if(sum > k) {
+                sum = sum - arr[left];
+                left++;
+            }
+            
+            maxSum = Math.max(maxSum, sum);
+        }
+        System.out.println(maxSum);
+    }
+}
