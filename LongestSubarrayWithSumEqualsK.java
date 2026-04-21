a6b69e72eeb70257ff9078481654bd13ee1715ef
Deepesh <deepeshkunkulol45@gmail.com>
2026-04-21 19:50:59 +0530

    Longest Subarray with Sum = K (with negatives)

---
+++ LongestSubarrayWithSumEqualsK.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+// Longest Subarray with Sum = K (with negatives)
+
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, -1, 5, -2, 3};
+        int k = 3;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int prefixSum = 0;
+        int maxLength = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            prefixSum += arr[i];
+
+            if (prefixSum == k) {
+                maxLength = i + 1;
+            }
+
+            if (map.containsKey(prefixSum - k)) {
+                int length = i - map.get(prefixSum - k);
+                maxLength = Math.max(maxLength, length);
+            }
+
+            if (!map.containsKey(prefixSum)) {
+                map.put(prefixSum, i);
+            }
+        }
+
+        System.out.println(maxLength);
+    }
+}
