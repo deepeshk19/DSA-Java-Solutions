9dd71ccc891a17c1caca17e0e5aee34f0e499e99
Deepesh <deepeshkunkulol45@gmail.com>
2026-04-27 18:56:40 +0530

    Reversed Linked List

---
+++ ReversedLinkedList.java
@@ -0,0 +1,17 @@
+// Reversed LinkedList
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode temp = head;
+        ListNode prev = null;
+        ListNode next = null;
+
+        while(temp != null) {
+            next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
+        }
+        return prev;
+    }
+}
